fdcae1eb2b56b5c0a2aa1a80fdf8b00ca9942af6
Brandon Mommsen <bmmommsen@alaska.edu>
2022-08-16 18:37:22 -0800

    Add Rapier3D plugin

--- elast/src/main.rs
+++ elast/src/main.rs
@@ -1,47 +1,50 @@
-// https://github.com/bevyengine/bevy/blob/main/examples/3d/3d_scene.rs
-//! A simple 3D scene with light shining over a cube sitting on a plane.
-
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
+        .add_plugin(RapierPhysicsPlugin::<NoUserData>::default())
+        .add_plugin(RapierDebugRenderPlugin::default())
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_startup_system(setup_world)
+        .add_startup_system(setup_player)
+        .add_startup_system(setup_camera)
         .run();
 }
 
-// set up a simple 3D scene
-fn setup(
+fn setup_world(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // plane
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
-        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
-        ..default()
-    });
-    // cube
-    commands.spawn_bundle(PbrBundle {
-        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
-        transform: Transform::from_xyz(0.0, 0.5, 0.0),
-        ..default()
-    });
-    // light
-    commands.spawn_bundle(PointLightBundle {
-        point_light: PointLight {
-            intensity: 1500.0,
-            shadows_enabled: true,
-            ..default()
-        },
-        transform: Transform::from_xyz(4.0, 8.0, 4.0),
-        ..default()
-    });
-    // camera
+    // ground
+    let (dimx, dimy, dimz) = (100., 0.1, 100.);
+    commands
+        .spawn()
+        .insert(Collider::cuboid(dimx, dimy, dimz))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
+}
+
+fn setup_player(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    // player will just be a ball for now
+    commands
+        .spawn()
+        .insert(RigidBody::Dynamic)
+        .insert(Collider::ball(0.5))
+        .insert(Restitution::coefficient(0.7))
+        .insert_bundle(TransformBundle::from(Transform::from_xyz(0.0, 2.0, 0.0)));
+}
+
+fn setup_camera(
+    mut commands: Commands,
+) {
     commands.spawn_bundle(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
+        transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..Default::default()
     });
 }
